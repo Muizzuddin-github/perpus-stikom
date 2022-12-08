4e5a43fbe28e7ca8b337ec838ee9d504fed1b430
Muizzuddin-github <muizuddin334@gmail.com>
2022-12-08 07:11:58 +0700

    tampilkan column isbn API terkenal

--- pages/api/buku/terkenal.ts
+++ pages/api/buku/terkenal.ts
@@ -11,7 +11,7 @@ export default async function(req: NextApiRequest, res: NextApiResponse<Data>){
     if(req.method !== 'GET') return res.status(405).json({status: "method not allowed",data: []})
 
     try{
-        const data = await sqlite3({pb: "peminjaman_buku"}).join({b: "buku"},'pb.kode_buku','b.kode_buku').join({d: "deskripsi"},'b.isbn','d.isbn').select('pb.id_pinjaman','d.judul_buku','d.gambar_buku','d.pengarang','d.jumlah_halaman').groupBy('d.judul_buku').count({jumlah_dipinjam : 'd.judul_buku'}).orderBy('jumlah_dipinjam','desc').limit(6)
+        const data = await sqlite3({pb: "peminjaman_buku"}).join({b: "buku"},'pb.kode_buku','b.kode_buku').join({d: "deskripsi"},'b.isbn','d.isbn').select('pb.id_pinjaman','d.isbn','d.judul_buku','d.gambar_buku','d.pengarang','d.jumlah_halaman').groupBy('d.judul_buku').count({jumlah_dipinjam : 'd.judul_buku'}).orderBy('jumlah_dipinjam','desc').limit(6)
         
         return res.status(200).json({status: "success",data})
     }catch(err: any){
