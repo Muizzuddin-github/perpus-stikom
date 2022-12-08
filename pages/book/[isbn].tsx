f97faaf70a3fe114d5fb492352d0a353cf7305b8
Muizzuddin-github <muizuddin334@gmail.com>
2022-12-08 07:34:06 +0700

    perbaikan url

--- pages/book/[isbn].tsx
+++ pages/book/[isbn].tsx
@@ -30,9 +30,7 @@ export default Book;
 
 export const getServerSideProps: GetServerSideProps = async function(context){
     const isbn = context.params?.isbn
-    const url = context.req.headers.referer || ''
-    const domainName = new URL(url)
-    const {data} = await(await fetch(`${domainName.origin}/api/buku/${isbn}`)).json()
+    const {data} = await(await fetch(`http://localhost:3000/api/buku/${isbn}`)).json()
     return {
         props: {detilBuku: data[0]}
     }
